a4bfdcc167dda0b628cfc68211d9eb7802c45ec1
Ianne Carl Bulilan <bulilaniannecarl@gmail.com>
2026-05-29 14:45:02 +0800

    fix: move outputFileTracingIncludes out of experimental

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["@sparticuz/chromium"],
+  outputFileTracingIncludes: {
+    "/api/export": ["./node_modules/@sparticuz/chromium/bin/**/*"],
+  },
 };
 
 export default nextConfig;
